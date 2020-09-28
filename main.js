4df750785f93cf2aeb75fb392b6dec7e87ad8d5a
Chanhee Jeong <chjeong530@gmail.com>
2020-09-28 15:58:21 +0900

    test

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 // echo '{"access_token": "", "image": "'"$(base64 sample_image.png)"'"}' | curl -X POST -d @- "http://keti.asuscomm.com:32222/function/imagestress"
 
+function uploadImgPreview(){
+    const toDataURL = url => fetch(url)
+        .then(response => response.blob())
+        .then(blob => new Promise((resolve, reject) => {
+            const reader = new FileReader()
+            reader.onloadend = () => resolve(reader.result)
+            reader.onerror = reject
+            reader.readAsDataURL(blob)
+        }))
 
+    toDataURL('items/sample_image.png')
+    .then(dataUrl => {
+        console.log('RESULT:', dataUrl.split(',')[1])
+    })
+}
